0f8d2909df08ff28d7a40a1a45a80bd41d5f4784
Jorge Villatoro <jorge@tomatocannon.com>
2009-12-30 23:11:24 -0600

    removed unnecessary junk

--- old/bootstrap/chumby-backup/cbob_cmd.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CBOB_CMD_H__
-#define __CBOB_CMD_H__
-
-#define CBOB_CMD_READ_DIGITALS 1
-
-#endif
--- old/bootstrap/flash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cp -f bin/firmware-flash.bin main.bin
-openocd -f openocd-flash.cfg
